fd8f7e7f230109da71bc19d76fa68547c0687031
ana kaprielian <ana.kaprielian@despegar.com>
2021-11-01 09:51:02 -0300

    Section3: Binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -7,13 +7,24 @@ import {registerLocaleData} from "@angular/common";
   // template: '<h1>Listado de personas</h1>\n' +
   //   '<app-persona></app-persona>\n' +
   //   '<app-persona></app-persona>'
-  //styleUrls: ['./personas.component.css']
-  styles: [`h1{
-    color: red;
-  }`]
+  styleUrls: ['./personas.component.css']
+  // styles: [`h1{
+  //   color: red;
+  // }`]
 })
 
 //hay que declararlo en app.module.ts
 export class PersonasComponent{
+  deshabilitar = false;
+  mensaje = 'No se ha agregado ninguna persona.'
+  titulo ='';
 
+  agregarPersona() {
+    this.mensaje = 'Persona agregada.'
+  }
+
+  modificarTitulo($event: Event) {
+    console.log('Entrando a método modificarTitulo.')
+    this.titulo = (<HTMLInputElement>$event.target).value;
+  }
 }
